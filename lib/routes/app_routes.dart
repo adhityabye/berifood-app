88640ac2edf3d8ee457364a15dc71480495e0bc1
Abinnnn <mr.adhityabayu@gmail.com>
2023-02-09 21:14:52 +0700

    app routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/routes/default_transitions.dart';
+
+import '../ui/login_page.dart';
+import '../ui/splash_screen.dart';
+import '../ui/introduction/tutorial_page_1.dart';
+import '../ui/introduction/tutorial_page_2.dart';
+import '../ui/introduction/tutorial_page_3.dart';
+
+class AppRoutes {
+  static List<GetPage> routes() => [
+        GetPage(
+          name: "/",
+          page: () => const SplashScreen(),
+        ),
+        GetPage(
+          name: "/introduction",
+          page: () => const TutorialPageOne(),
+        ),
+        GetPage(
+          name: "/introduction2",
+          page: () => const TutorialPageTwo(),
+        ),
+        GetPage(
+          name: "/introduction3",
+          page: () => const TutorialPageThree(),
+        ),
+        GetPage(
+          name: "/login",
+          page: () => const LoginPage(),
+        ),
+      ];
+}
